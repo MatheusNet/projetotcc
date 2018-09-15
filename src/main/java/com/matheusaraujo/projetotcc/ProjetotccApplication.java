c3da0d5b5b45455545d03e16ccb4724722444a2c
Matheus de Arujo <matheus.net10@outlook.com>
2018-09-15 20:44:32 -0300

    PROJETO CRIADO

---
+++ src/main/java/com/matheusaraujo/projetotcc/ProjetotccApplication.java
@@ -0,0 +1,12 @@
+package com.matheusaraujo.projetotcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotccApplication.class, args);
+	}
+}
