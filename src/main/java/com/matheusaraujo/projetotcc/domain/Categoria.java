a59d194c85e56e39c43e0ed6c51671ede87b424e
Matheus de Arujo <matheus.net10@outlook.com>
2018-09-15 23:28:27 -0300

    TESTANDO A PRIMEIRA CLASSE DE DOMINIO

---
+++ src/main/java/com/matheusaraujo/projetotcc/domain/Categoria.java
@@ -0,0 +1,73 @@
+package com.matheusaraujo.projetotcc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+
+	//CRIANDO OS PARÂMETROS
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nome;
+	
+	//CONSTRUTOR VAZIO
+	public Categoria() {
+		
+	}
+	
+	//CONSTRUTOR COM PARÂMETROS
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	//GERA UM CÓDIGO NUMÉRICO DIFERENTE PARA CADA OBJETO
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	//COMPARAÇÃO ENTRE DOIS OBJETOS
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+		
+	
+}
--- src/main/java/com/matheusaraujo/projetotcc/resources/CategoriaResource.java
+++ src/main/java/com/matheusaraujo/projetotcc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.matheusaraujo.projetotcc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matheusaraujo.projetotcc.domain.Categoria;
 
 @RestController// NOTAÇÃO INDICATIVA DE UM CONTROLLER REST
 @RequestMapping(value = "/categorias")//END POINT INFORMADO 
@@ -10,7 +15,20 @@ public class CategoriaResource {
 
 	//ASSOCIANDO ESTE MÉTODO A UM VERBO HTTP
 	@RequestMapping(method = RequestMethod.GET)
-	public String Listar() {
-		return "RESTE ESTÁ FUNCIONANDO";
+	public List<Categoria> Listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		/*CRIANDO UM A LISTA DE CATEGORIAS
+		 * O LIST É UMA INTERFACE, E POR ISSO NÃO PODE
+		 * SER INSTANCIADA. ENTÃO POSSO INSTANCIAR UMA CLASSE
+		 * QUE IMPLEMENTE ESTA INTERFACE COMO É O CASO DE ARRAYLIST
+		 *  
+		 *  */
+		List<Categoria>  lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
